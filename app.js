c544f65ce7daf94c65a0ed67b42bea602eba461e
Sofia Monasterio <sofimonasterio93@gmail.com>
2021-05-28 18:33:20 -0300

    Tarea POO 24-05

---
+++ app.js
@@ -0,0 +1,89 @@
+//**Objeto Persona**
+/*Crear un objeto llamado persona que contenga los siguientes atributos:
+Nombre
+Edad
+DNI
+Domicilio
+Hijos (cantidad)
+Profesión
+Agregar Métodos:
+Saludar ( mensaje de presentación en navegador o consola)
+Listar (muestra la lista con los datos de la persona)*/
+
+/*let persona = {
+  nombre: "Sofia",
+  edad: 27,
+  DNI: 37905751,
+  domicilio: "Leloir 645",
+  hijos: 0,
+  prefesion: "Estudiante",
+  saludar: function () {
+    console.log(`Hola ${this.nombre}`);
+  },
+  listar: function () {
+    for (let atributo in persona) {
+      console.log(`${atributo}: ${persona[atributo]}`);
+    }
+  },
+};*/
+
+//----------------------------------------------------------------------------------
+//**Arreglo de objetos: Películas**
+
+/*Crear un arreglo de objetos que contenga datos de películas como:
+- Título
+- Género
+- Año
+- sinopsis
+- Crea una función que permita mostrar en una lista 
+cada película con sus datos.*/
+
+
+/*let peliculas = [
+  {
+    titulo: "El Padrino",
+    genero: "Drama, Mafia",
+    año: 1972,
+    sinopsis:
+      "Las familias mafiosas de Nueva York entran en conflicto cuando el capo de una de las más poderosas, Vito Corleone, se opone a que la Cosa Nostra entre en el negocio del tráfico de drogas. Como consecuencia, sufre un atentado que le deja al borde de la muerte. Para vengar el golpe y salvar a la familia, Michael Corleone, su hijo predilecto, quien siempre había estado al margen de los negocios de la familia, debe meterse de lleno en el sucio mundo del crimen.",
+  },
+  {
+    titulo: "El mago de Oz",
+    genero: "Ciencia Ficción",
+    año: "1939",
+    sinopsis:
+      "Dorothy, es una niña huérfana que se siente infeliz en la granja de sus tíos en Kansas. Desea viajar y su sueño se hace realidad cuando es arrastrada por un torbellino mágico al mundo de Oz. Ahí con la ayuda de un espantapájaros, un hombre de hojalata y un león cobarde tratará de encontrar al Mago de la Ciudad Esmeralda. Eso si la malvada bruja del Oeste se lo permite.",
+  },
+  {
+    titulo: "Ciudadano Kane",
+    genero: "Drama, Misterio",
+    año: "1941",
+    sinopsis:
+      "Charles Foster Kane muere en su inmenso castillo de Xanadú. Cuando al periodista Herbert Carter le encargan investigar su misteriosa vida, empieza con la última palabra que apenas pudo pronunciar el magnate antes de morir, Rosebud.",
+  },
+  {
+    titulo: "Cadena Perpetua",
+    genero: "Drama, Misterio, Policial",
+    año: "1994",
+    sinopsis:
+      "Andy ingresa en una prisión de alta seguridad para cumplir cadena perpetua por el asesinato de su mujer y del amante de esta. Allí se gana el respeto de los presos y de Red, el jefe del mercado negro de la cárcel, del que también participa el alcaide. Andy se entera de que el verdadero asesino de su mujer está encerrado en otro penal, por lo que pide que se reabra su caso.Andy ingresa en una prisión de alta seguridad para cumplir cadena perpetua por el asesinato de su mujer y del amante de esta. Allí se gana el respeto de los presos y de Red, el jefe del mercado negro de la cárcel, del que también participa el alcaide. Andy se entera de que el verdadero asesino de su mujer está encerrado en otro penal, por lo que pide que se reabra su caso.Andy ingresa en una prisión de alta seguridad para cumplir cadena perpetua por el asesinato de su mujer y del amante de esta. Allí se gana el respeto de los presos y de Red, el jefe del mercado negro de la cárcel, del que también participa el alcaide. Andy se entera de que el verdadero asesino de su mujer está encerrado en otro penal, por lo que pide que se reabra su caso.Andy ingresa en una prisión de alta seguridad para cumplir cadena perpetua por el asesinato de su mujer y del amante de esta. Allí se gana el respeto de los presos y de Red, el jefe del mercado negro de la cárcel, del que también participa el alcaide. Andy se entera de que el verdadero asesino de su mujer está encerrado en otro penal, por lo que pide que se reabra su caso.",
+  },
+  {
+    titulo: "Pulp Fiction",
+    genero: "Mafia, Drama",
+    año: "1994",
+    sinopsis:
+      "Vincent Vega y Jules Winnfield son un par de matones que, cumpliendo órdenes de su jefe Marsellus, recuperan un misterioso maletín de unos ladrones aficionados. Por otra parte, Marsellus pide a Vincent que cuide de su mujer Mia mientras él se va de viaje. Su camino se cruzará con el de un boxeador que rehúsa dinero de la mafia por perder un combate. ",
+  },
+];
+
+peliculas.forEach(function (pelicula) {
+    console.log(`
+  - Título: ${pelicula.titulo} 
+  - Género: ${pelicula.genero} 
+  - Año: ${pelicula.año} 
+  - Sinópsis: ${pelicula.sinopsis} `);
+    console.log("----------------------------------");
+  });
+
+*/
